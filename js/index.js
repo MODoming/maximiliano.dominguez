51961c3c9e80e452f8b2dbd17b4b8cb7e1efd4c2
Max <maxi.dmgz@gmail.com>
2023-08-09 15:45:45 -0300

    Probando nuevas cosas con PHP

--- js/index.js
+++ js/index.js
@@ -1,23 +1,4 @@
-jQuery('document').ready(function($){
-    var menuBtn = $('.navegar'),
-        menu = $('.menu ul'),
-        menuDeplegado = $('.menu');
-
-    menuBtn.click(function(){
-        if (menu.hasClass('show')){
-            menu.removeClass('show');
-        } else {
-            menu.addClass('show');
-        };        
-    });
-    menuDeplegado.click(function(){
-        if (menu.hasClass('show')){
-            menu.removeClass('show');
-        } else {
-            menu.addClass('show');
-        };
-    });
-
+document.addEventListener("DOMContentLoaded", function () {
     var formulario = document.getElementById("formulario");
 
     formulario.addEventListener("submit", function (event) {
@@ -42,15 +23,5 @@ jQuery('document').ready(function($){
         .catch(function (error) {
             console.error("Error:", error);
         });
-
-        // Enviar los datos del formulario a Google Apps Script
-        var scriptURL = 'https://script.google.com/macros/s/AKfycby-It0AyVUO6J8xlT0lmN9W9jxvWZY5mE2P4UWPYBDrUzVg_9qPou6lkmcx98GTdPz9pw/exec';
-        fetch(scriptURL, {
-            method: 'POST',
-            body: formData,
-        })
-        .then(response => console.log('Formulario enviado a Google Apps Script:', response))
-        .catch(error => console.error('Error al enviar el formulario a Google Apps Script:', error));
     });
 });
-
