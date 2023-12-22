c3d69ef6219a1dc19a204c77dc4016fb9b18d46d
Max <maxi.dmgz@gmail.com>
2023-12-21 23:13:00 -0300

    Modificando el contacto

--- js/index.js
+++ js/index.js
@@ -17,40 +17,4 @@ jQuery('document').ready(function($){
             menu.addClass('show');
         };
     });
-
-    var formulario = document.getElementById("formulario");
-
-    formulario.addEventListener("submit", function (event) {
-        event.preventDefault(); // Evitar el envío del formulario por defecto
-
-        var formData = new FormData(formulario);
-
-        fetch(formulario.action, {
-            method: "post",
-            body: formData
-        })
-        .then(function (response) {
-            return response.text();
-        })
-        .then(function (result) {
-            console.log(result); // Mostrar resultado en la consola
-            if (result.includes("enviado correctamente")) {
-                // Restablecer los valores de los campos del formulario
-                formulario.reset();
-            }
-        })
-        .catch(function (error) {
-            console.error("Error:", error);
-        });
-
-        // Enviar los datos del formulario a Google Apps Script
-        
-        var scriptURL = 'https://script.google.com/macros/s/AKfycbz2WzmxbZzvSwxo-x-iYyORa_O5vOWzBEfV7fIbpl55D0xwBU9aNSN9EcUw5E1sdvOr/exec';
-        fetch(scriptURL, {
-            method: 'POST',
-            body: formData,
-        })
-        .then(response => console.log('Formulario enviado a Google Apps Script:', response))
-        .catch(error => console.error('Error al enviar el formulario a Google Apps Script:', error));
-    });
 });
